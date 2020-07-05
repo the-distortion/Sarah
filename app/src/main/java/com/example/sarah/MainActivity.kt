9b8189bf0a99cd8846fa2de0d82d68ad992acf50
the-distortion <soham86das@gmail.com>
2020-07-05 21:05:10 +0530

    Added Frame Layout and Fragment to home page :(

--- app/src/main/java/com/example/sarah/MainActivity.kt
+++ app/src/main/java/com/example/sarah/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.sarah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+//import androidx.drawerlayout.widget.DrawerLayout
+//import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var home_layout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        home_layout = findViewById(R.id.home_layout)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame, Home())
+            .commit()
+
     }
 }
