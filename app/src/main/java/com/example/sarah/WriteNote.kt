c6ef5faf171412973abd5ad000122f7a298f7437
the-distortion <soham86das@gmail.com>
2020-07-12 04:18:06 +0530

    Reset note body

--- app/src/main/java/com/example/sarah/WriteNote.kt
+++ app/src/main/java/com/example/sarah/WriteNote.kt
@@ -1,12 +1,15 @@
 package com.example.sarah
 
+import android.content.DialogInterface
 import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.fragment_notes.*
 import kotlinx.android.synthetic.main.fragment_write_note.*
 
@@ -23,6 +26,29 @@ class WriteNote : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        img_btn_reset.setOnClickListener {
+
+            val alertDialog: AlertDialog? = activity?.let {
+                val builder = AlertDialog.Builder(it)
+                builder.apply {
+                    setTitle(R.string.reset_note_dialog_head)
+                    setMessage(R.string.reset_note_dialog_message)
+                    setPositiveButton(R.string.ok,
+                        DialogInterface.OnClickListener { _, _ ->
+                            Toast.makeText(activity!!, "Resetting Note Body", Toast.LENGTH_LONG).show()
+                            activity!!.findViewById<EditText>(R.id.note_body).text.clear()
+                        })
+                    setNegativeButton(R.string.dont_reset_note,
+                        DialogInterface.OnClickListener { _, _ ->
+                            Toast.makeText(activity!!, "Better be careful from the next time!", Toast.LENGTH_LONG).show()
+                        })
+                }
+                val create = builder.create()
+                create
+            }
+
+            alertDialog?.show()
+        }
 
         btn_save_note.setOnClickListener {
 
