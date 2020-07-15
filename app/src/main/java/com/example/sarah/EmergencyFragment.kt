8231a5a7c6e264b02707eb4786fa3bfa02e601e5
the-distortion <soham86das@gmail.com>
2020-07-16 04:32:38 +0530

    Emergency Call's contact management done. Call and delete, not implemented and not working, respectively.

--- app/src/main/java/com/example/sarah/EmergencyFragment.kt
+++ app/src/main/java/com/example/sarah/EmergencyFragment.kt
@@ -11,29 +11,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_emergency.*
 
 class EmergencyFragment : Fragment() {
 
 //    var arr = arrayListOf<String>(*context?.fileList()!!)
-    val contactSharedPreference = activity?.getSharedPreferences(getString(R.string.emergency_head), Context.MODE_PRIVATE)
+    var contactSharedPreference = activity?.getSharedPreferences(getString(R.string.emergency_head), Context.MODE_PRIVATE)
     val REQUEST_PHONE_NUMBER = 1
     lateinit var contactUri: Uri
+    lateinit var adapterArr: MutableList<String>
+    lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        contactSharedPreference = activity?.getSharedPreferences(getString(R.string.emergency_head), Context.MODE_PRIVATE)
+        adapterArr = contactSharedPreference!!.all.keys.toMutableList()
         return inflater.inflate(R.layout.fragment_emergency, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = ArrayAdapter<String>(context!!, R.layout.contact_items,
-            contactSharedPreference!!.all.keys.toTypedArray())
-        emergency_contact_list.adapter = adapter
+        println(contactSharedPreference)
+        if(contactSharedPreference != null) {
+            adapter = ArrayAdapter<String>(context!!, R.layout.contact_items, R.id.contact_name, adapterArr)
+            emergency_contact_list.adapter = adapter
+        }
 
         btn_add_emergency_contact.setOnClickListener {
 
@@ -57,18 +65,47 @@ class EmergencyFragment : Fragment() {
     }
 
     private fun getPhoneNumber() {
-        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)
-        var cursor = context!!.contentResolver.query(contactUri, projection, null, null, null)
+        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER)
+        val cursor = context!!.contentResolver.query(contactUri, projection, null, null, null)
 
         if(cursor!!.moveToFirst()){
             val phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-            val contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
-
-            with (contactSharedPreference!!.edit()) {
+            val contactName= cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+            println(contactName)
+            println(phoneNumber)
+//            if(cursor.moveToNext()) {
+//                phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+//                println(phoneNumber)
+//                with(contactSharedPreference!!.edit()) {
+//                    putLong(contactName, phoneNumber.toLong())
+//                    commit()
+//                }
+//                Toast.makeText(context, "$contactName : $phoneNumber", Toast.LENGTH_LONG).show()
+//            }
+            println("contactSharedPreference : $contactSharedPreference")
+            with(contactSharedPreference!!.edit()) {
                 putLong(contactName, phoneNumber.toLong())
                 commit()
             }
+            Toast.makeText(context, "$contactName >> ${contactSharedPreference!!.getLong(contactName, -1)}", Toast.LENGTH_LONG).show()
+            adapterArr.add(contactName)
+            adapter.notifyDataSetChanged()
         }
+        cursor.close()
+        contactSharedPreference!!.all.forEach {
+            println("${it.key} => ${it.value}")
+        }
+    }
+
+    public fun callContact(view: TextView)
+    {
+        Toast.makeText(context, view.text, Toast.LENGTH_LONG).show()
+    }
+
+    public fun deleteContact(view: TextView)
+    {
+        Toast.makeText(context, "${view.text} deleted!", Toast.LENGTH_LONG).show()
+        view.visibility = View.GONE //false delete
     }
 
 }
